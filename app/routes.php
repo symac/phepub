7aa34d979e40272805cc21cad7a80dfefddc2328
symac <web@geobib.fr>
2018-05-16 05:52:27 +0200

    Add a link to update all lessons on home.

--- app/routes.php
+++ app/routes.php
@@ -1,4 +1,4 @@
 <?php
 	$app->get('/', "Phepub\Controller\HomeController::indexAction");
 	$app->get('/build-epub/{lessonCode}', "Phepub\Controller\HomeController::buildEpubAction")->bind("generate")->value("lessonCode", "all")->assert('lessonCode', '^[\w\-\._/]+');;
-	$app->get('/check-for-updates', "Phepub\Controller\UpdateController::checkNewLessonsAction");
+	$app->get('/check-for-updates', "Phepub\Controller\UpdateController::checkNewLessonsAction")->bind("update-cache-lessons");
